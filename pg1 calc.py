65f3d5378bbb55aea0cc9764c7d9b698baa31064
Swetha V <swethashilpa28_bec28@mepcoeng.ac.in>
2026-08-17 09:40:38 +0430

    Add simple calculator script

---
+++ pg1 calc.py
@@ -0,0 +1,13 @@
+#SWETHA
+#calc
+x1=int(input("enter number"))
+x2=int(input('enter num2'))
+ch=int(input('enter ur choice i.sum,2.sub,3.mul,4.div'))
+if ch==1:
+    print('sum',x1+x2)
+elif ch==2:
+    print('sub',x1-x2)
+elif ch==3:
+    print('mul',x1*x2)
+elif ch==4:
+    print('div',float(x1/x2))
